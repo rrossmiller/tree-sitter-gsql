8dfd7f9f17aebf5c1e3f8b7be9a0c2cb539fe568
Rossmiller, Robert <robert.rossmiller@optum.com>
2023-03-07 18:08:35 -0500

    add build script

--- build.sh
+++ build.sh
@@ -1,1 +1,12 @@
-# TODO build script
+cp grammar.js grammar_case_insensitive.js
+# pase the file and check the status
+./node_modules/.bin/tree-sitter parse $file &&
+    # convert the grammar to a case-sensitive version to allow for sytax nodes to show up
+    # this requires a keyword preprocessor in the formatter
+    echo 'convert the grammar to a case-sensitive version' &&
+    python3 case.py &&
+    # regenerate the library with the case-sensitive grammar
+    echo 'regenerate the library with the case-sensitive grammar' &&
+    ./node_modules/.bin/tree-sitter generate
+
+# install the library
